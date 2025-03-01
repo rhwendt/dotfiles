0d1bc33bdc6d178f1ad06bb1309aac25ded919dc
Ryan Wendt <rhwendt@gmail.com>
2025-02-28 23:59:47 -0500

    updates WIP

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,6 +4,7 @@
 vim.go.background = "dark"
 vim.g.python3_host_prog = "$HOME/.pyenv/versions/$(pyenv global)/bin/python"
 vim.opt.colorcolumn = "88"
+vim.opt.updatetime = 50
 
 -- disable ai crap
 vim.g.ai_cmp = false
--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "snacks.nvim",
+    "folke/snacks.nvim",
     opts = {
       scroll = { enabled = false },
       dashboard = {
@@ -19,6 +19,24 @@ return {
 ]],
         },
       },
-    }
+      picker = {
+        layout = {
+          box = "vertical",
+          backdrop = false,
+          row = -1,
+          width = 0,
+          height = 0.4,
+          border = "top",
+          title = " {title} {live} {flags}",
+          title_pos = "left",
+          { win = "input", height = 1, border = "bottom" },
+          {
+            box = "horizontal",
+            { win = "list", border = "none" },
+            { win = "preview", title = "{preview}", width = 0.6, border = "left" },
+          },
+        },
+      },
+    },
   },
 }
