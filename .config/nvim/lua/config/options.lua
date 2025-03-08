88b1141d95ff790d095fae527e16562dc2a581e9
Ryan Wendt <rhwendt@gmail.com>
2025-03-07 22:15:57 -0500

    add lazygit config

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.go.background = "dark"
-vim.g.python3_host_prog = $HOME."/.pyenv/versions/".${pyenv global}."/bin/python"
+vim.g.python3_host_prog = "$HOME/.pyenv/versions/$(pyenv global)/bin/python"
 vim.opt.colorcolumn = "88"
 vim.opt.updatetime = 50
 
