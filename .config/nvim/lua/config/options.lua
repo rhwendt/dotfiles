ddfaa363a3c8bb58e491a32b94102b77c88f230a
Ryan Wendt <rhwendt@gmail.com>
2025-03-07 23:01:10 -0500

    add vim python program

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,8 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local handle = io.popen("pyenv global", "r")
+if handle ~= nil then
+  local output = handle:read("*a")
+  io.close(handle)
+  output = output:gsub("\n[^\n]*$", "")
+  vim.g.python3_host_prog = os.getenv("HOME") .. "/.pyenv/versions/" .. output .. "/bin/python"
+end
 vim.go.background = "dark"
-vim.g.python3_host_prog = "$HOME/.pyenv/versions/$(pyenv global)/bin/python"
 vim.opt.colorcolumn = "88"
 vim.opt.updatetime = 50
 
